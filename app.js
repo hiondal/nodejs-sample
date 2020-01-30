5aca5e17dcf1d95458bffe39cb2fdcec8e30bc8e
c-mac <c-mac@mac.com>
2020-01-30 11:39:18 +0900

    merge commit

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
     res.send('Hello World! hook kk');
-=======
-    res.send('Hello World! modify');
->>>>>>> 1add88a92cb439864ed5cc5affb2f8dcb0c020a9
 });
 
 const port = process.env.PORT || 5000;
